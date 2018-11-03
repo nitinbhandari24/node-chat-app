ad22ebaa9d87a3019d4730d404a37b98363ba884
NiTiN <24nitin1994@gmail.com>
2018-11-03 19:37:23 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
@@ -25,21 +26,18 @@ io.on('connection', (socket) => {
   //   console.log('createEmail', newEmail);
   // })
 
-// TO message to a particular user
-  // socket.emit('newMessage', {
-  //   from: 'nitin',
-  //   text: 'Message generated',
-  //   createdAt: 123
-  // });
+//TO message to a particular user
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'))
+
 
   socket.on('createMessage', (message) => {
-    console.log("createMessage", message);
-// TO message to all user(broadcast)
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime
-    })
+    console.log('createMessage', message);
+
+//TO message to all user from one user
+    io.emit('newMessage', generateMessage(message.from, message.text))
+
   });
 
   socket.on('disconnect', () => {
